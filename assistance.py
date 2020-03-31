19ba46b81a3f53899f76528ef43392b3675e6d89
Bigyan Timislina <42671917+bigyanic@users.noreply.github.com>
2020-03-31 13:14:33 +0545

    create assistance.py

---
+++ assistance.py
@@ -0,0 +1,9 @@
+responsepair=[
+['(hi|hello|hey)', ['hey there,How can i help you', 'Namaste,How can I help you', 'Namaste']],
+    ['(what is my name?|do you know me?|who am i?)', ['sorry ,i dont know your name .What is your name?',
+                                                  'i am not taught to know your name just by reading your "hi" message, tell me your name i will remember you']],
+['who created you?', ['bigyan timilsina created me.']],
+['my name is (.*)', ['hi,%1 nice to meet you']],
+['who are you?|what is your name?',
+ [' i am corona virus, i am gonna kill you.funny right?', 'i am your assistant. tell me how can i help you?']],
+['(what is my sister name?|what is my sister name)', ['She is prabidhi.She is so beautiful ']],]
